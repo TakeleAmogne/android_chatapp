10500bb5ee17b2b0bf9da373f4328a66d4f44cac
alhudaghifari <alhudaghifari@gmail.com>
2018-02-17 22:29:46 +0700

    layouting and add java code

---
+++ Pepechat/app/src/main/java/com/alhudaghifari/pepechat/UserDetails.java
@@ -0,0 +1,11 @@
+package com.alhudaghifari.pepechat;
+
+/**
+ * Created by ghifar on 14/02/18.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
